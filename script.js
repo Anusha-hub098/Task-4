0c75529010502caffd4e5650cc262b4ec5d2fd8d
Anusha <anushadurgasi18@gmail.com>
2025-10-05 12:49:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openTodo() {
+  // open the todo app page inside the folder
+  window.location.href = "todo-app/index.html";
+}
+
+function openProduct() {
+  // open the product page
+  window.location.href = "product-page/index.html";
+}
